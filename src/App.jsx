fbebb7addf071fbeed6d8a27bb1e0b6bd03c949c
Alvin Joshua Caponpon <143042393+juki-juki@users.noreply.github.com>
2023-12-09 00:26:44 +0800

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
+// src/App.js
 import React, { useState } from 'react';
-import TodoList from './Todolist.jsx';
-
+import TodoList from './Todolist';
 
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build a Todo app', completed: true },
+    {
+      id: 1,
+      text: 'Learn React',
+      completed: false,
+      dueDate: '2023-12-31',
+      description: 'Start learning React for web development.',
+    },
+    {
+      id: 2,
+      text: 'Build a Todo app',
+      completed: true,
+      dueDate: '2023-12-15',
+      description: 'Create a simple Todo app using React.',
+    },
   ]);
 
   const handleToggle = (id) => {
@@ -17,10 +28,16 @@ const App = () => {
     );
   };
 
-  const handleAdd = (text) => {
+  const handleAdd = (text, dueDate, description) => {
     setTodos((prevTodos) => [
       ...prevTodos,
-      { id: prevTodos.length + 1, text, completed: false },
+      {
+        id: prevTodos.length + 1,
+        text,
+        completed: false,
+        dueDate: dueDate || null,
+        description: description || '',
+      },
     ]);
   };
 
@@ -38,9 +55,21 @@ const App = () => {
     );
   };
 
+  const calculateProgress = () => {
+    const totalTasks = todos.length;
+    const completedTasks = todos.filter((todo) => todo.completed).length;
+
+    return totalTasks === 0 ? 0 : Math.floor((completedTasks / totalTasks) * 100);
+  };
+
   return (
     <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded">
       <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <div className="mb-4">
+        <progress className="w-full" value={calculateProgress()} max="100">
+          {calculateProgress()}%
+        </progress>
+      </div>
       <TodoList
         todos={todos}
         onToggle={handleToggle}
@@ -50,15 +79,43 @@ const App = () => {
       <div className="mt-4">
         <input
           type="text"
-          placeholder="Add a new task"
-          className="p-2 border rounded"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && e.target.value.trim()) {
-              handleAdd(e.target.value.trim());
-              e.target.value = '';
+          placeholder="Task name"
+          className="p-2 border rounded mb-2"
+        />
+        <input
+          type="date"
+          placeholder="Due date"
+          className="p-2 border rounded mb-2"
+        />
+        <textarea
+          placeholder="Task description"
+          className="p-2 border rounded mb-2"
+        ></textarea>
+        <button
+          onClick={() => {
+            // Extract values from input fields
+            const nameInput = document.querySelector('input[placeholder="Task name"]');
+            const dueDateInput = document.querySelector('input[placeholder="Due date"]');
+            const descriptionInput = document.querySelector('textarea[placeholder="Task description"]');
+            
+            const name = nameInput.value.trim();
+            const dueDate = dueDateInput.value.trim();
+            const description = descriptionInput.value.trim();
+
+            // Check if name is not empty
+            if (name) {
+              handleAdd(name, dueDate, description);
+
+              // Clear input fields
+              nameInput.value = '';
+              dueDateInput.value = '';
+              descriptionInput.value = '';
             }
           }}
-        />
+          className="p-2 bg-blue-500 text-white rounded"
+        >
+          Add Task
+        </button>
       </div>
     </div>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,64 +1,67 @@
+// src/Todo.js
 import React, { useState } from 'react';
+import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
 
 const Todo = ({ todo, onToggle, onDelete, onRename }) => {
-    const [isEditing, setEditing] = useState(false);
-    const [newText, setNewText] = useState(todo.text);
-  
-    const handleEdit = () => {
-      setEditing(true);
-    };
-  
-    const handleSave = () => {
-      onRename(todo.id, newText);
-      setEditing(false);
-    };
+  const [isEditing, setEditing] = useState(false);
+  const [newText, setNewText] = useState(todo.text);
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    onRename(todo.id, newText);
+    setEditing(false);
+  };
+
   return (
-    <div className="flex items-center mb-2">
-      <input
-        type="checkbox"
-        checked={todo.completed}
-        onChange={() => onToggle(todo.id)}
-        className="mr-2"
-      />
-      {isEditing ? (
-        <>
-          <input
-            type="text"
-            value={newText}
-            onChange={(e) => setNewText(e.target.value)}
-            className="p-1 border rounded"
-          />
-          <button
-            onClick={handleSave}
-            className="ml-2 px-2 py-1 bg-blue-500 text-white rounded"
-          >
-            Save
-          </button>
-        </>
-      ) : (
-        <>
-          <span
-            style={{
-              textDecoration: todo.completed ? 'line-through' : 'none',
-            }}
-            className="mr-2"
-          >
-            {todo.text}
-          </span>
-          <button
-            onClick={handleEdit}
-            className="px-2 py-1 bg-green-500 text-white rounded"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => onDelete(todo.id)}
-            className="ml-2 px-2 py-1 bg-red-500 text-white rounded"
-          >
-            Delete
-          </button>
-        </>
-      )}
+    <div className="flex items-center mb-4">
+      <div className="flex-grow">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => onToggle(todo.id)}
+          className="mr-2"
+        />
+        {isEditing ? (
+          <>
+            <input
+              type="text"
+              value={newText}
+              onChange={(e) => setNewText(e.target.value)}
+              className="p-1 border rounded"
+            />
+            <button
+              onClick={handleSave}
+              className="ml-2 px-2 py-1 bg-blue-500 text-white rounded"
+            >
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <span
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+              }}
+              className="mr-2"
+            >
+              {todo.text}
+            </span>
+            <span className="text-gray-500 mr-2">{todo.dueDate}</span>
+            <PencilIcon
+              onClick={handleEdit}
+              className="w-6 h-6 text-green-500 cursor-pointer"
+            />
+            <TrashIcon
+              onClick={() => onDelete(todo.id)}
+              className="w-6 h-6 text-red-500 cursor-pointer ml-2"
+            />
+          </>
+        )}
+        <p className="text-gray-500 mt-2">{todo.description}</p>
+      </div>
     </div>
   );
 };
